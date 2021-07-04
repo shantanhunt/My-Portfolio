033de30c7b1a8d6b0245564c73759347cabcc71a
shantanhunt <shantanusontakke015@gmail.com>
2021-07-04 05:03:54 -0700

    Removed resume button

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
